1b94f7c7c2dc13bd7db3b2a0faf90ed0f9dfa069
moustafasamir21 <mustafa.samir@queentechsolutions.net>
2021-11-14 18:59:31 +0200

    agencies page

--- application/views/user/agents.php
+++ application/views/user/agents.php
@@ -1,82 +1,235 @@
-    <?php
-
-  $category = $this->admin_model->get_all_category();
-
-?>
 <!--=================================
-Browse properties Categories -->
+Listing – list view -->
 <section class="space-ptb">
   <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-lg-8">
-        <div class="section-title text-center">
-          <h2>Browse by category</h2>
-          <p>To browse and buy in your areas of interest, look for properties by category.</p>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="section-title mb-3 mb-lg-4">
+          <h2><span class="text-primary">156</span> Results</h2>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="property-filter-tag">
+          <ul class="list-unstyled">
+            <li><a href="#">Duplex <i class="fas fa-times-circle"></i> </a></li>
+            <li><a class="filter-clear" href="#">Reset Search <i class="fas fa-redo-alt"></i> </a></li>
+          </ul>
         </div>
       </div>
     </div>
     <div class="row">
-      <div class="col-12">
-        <div class="category">
-          <ul class="list-unstyled mb-0">
-          <?php if(isset($category)){ $cnt=1; ?>  
-	         <?php foreach($category as $row) { ?>
-	            <li class="category-item">
-	              <a href="<?php echo base_url('categories/').$row->id?>">
-	                <div class="category-icon">
-	                  <i class="flaticon-building-2"></i>
-	                </div>
-	                <h6 class="mb-0"><?=$row->c_name?></h6>
-	                <span>(<?=count_category($row->id)?>)</span>
-	              </a>
-	            </li>
-		    <?php } ?>
-         <?php } ?>
-   
+      <div class="col-lg-12 col-md-12">
+        <div class="property-filter d-sm-flex">
+          <ul class="property-short list-unstyled d-sm-flex mb-0">
+            <li>
+              <form class="form-inline">
+                <div class="form-group d-lg-flex d-block mb-sm-0 mb-3">
+                  <label class="justify-content-start mb-2 mb-sm-0">Short by:</label>
+                  <div class="short-by">
+                    <select class="form-control basic-select">
+                      <option>Date new to old</option>
+                      <option>Price Low to High</option>
+                      <option>Price High to Low</option>
+                      <option>Date Old to New</option>
+                      <option>Date New to Old</option>
+                    </select>
+                  </div>
+                </div>
+              </form>
+            </li>
           </ul>
+          <ul class="property-view-list list-unstyled d-flex mb-0">
+            <li>
+              <form class="form-inline">
+                <div class="form-group d-lg-flex d-block mb-3 mb-sm-0">
+                  <label class="justify-content-start pe-2 mb-sm-0 mb-2">Sort by: </label>
+                  <div class="short-by">
+                    <select class="form-control basic-select">
+                      <option>12</option>
+                      <option>18 </option>
+                      <option>24 </option>
+                      <option>64 </option>
+                      <option>128</option>
+                    </select>
+                  </div>
+                </div>
+              </form>
+            </li>
+            <li><a href="index-half-map.html"><i class="fas fa-map-marker-alt fa-lg"></i></a></li>
+            <li><a class="property-list-icon active" href="agency-list.html">
+              <span></span>
+              <span></span>
+              <span></span>
+            </a></li>
+            <li><a class="property-grid-icon" href="agency-grid.html">
+              <span></span>
+              <span></span>
+              <span></span>
+            </a></li>
+          </ul>
+        </div>
+        <div class="agent agent-03 mt-4">
+          <div class="row g-0">
+            <div class="col-lg-4 text-center border-end">
+              <div class="d-flex flex-column h-100">
+                <div class="agent-avatar p-3 my-auto">
+                  <img class="img-fluid rounded-circle" src="<?=base_url('website')?>/images/agency/01.png" alt="">
+                </div>
+                <div class="agent-listing text-center mt-auto">
+                  <a href="#"><strong class="text-primary me-2 d-inline-block">15</strong>Listed Properties </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-8">
+              <div class="d-flex h-100 flex-column">
+                <div class="agent-detail">
+                  <div class="d-sm-flex">
+                    <div class="agent-name">
+                      <h5> <a href="agency-detail.html">Duplex estates</a></h5>
+                      <span><i class="fas fa-map-marker-alt fa-xs pe-2"></i> Virginia drive temple hills</span>
+                    </div>
+                    <div class="agent-social ms-auto mt-2 mt-sm-0">
+                      <ul class="list-unstyled list-inline">
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i> </a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i> </a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin"></i> </a></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="agent-info">
+                    <p class="mt-3 mb-3">Figure out what you want, put a plan together to achieve it, understand the cost, believe in yourself then.</p>
+                    <ul class="list-unstyled mb-0">
+                      <li><strong>Office: </strong>(123) 345-6789</li>
+                      <li><strong>Fax: </strong>(123) 345-6789</li>
+                      <li><strong>Email: </strong>support@realvilla.demo </li>
+                    </ul>
+                    <ul class="list-unstyled mb-0">
+                      <li><strong>Mobile: </strong>(456) 478-2589</li>
+                      <li><strong>WhatsApp: </strong>(456) 478-2589</li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="agent-button">
+                  <a class="btn btn-light btn-lg d-grid" href="agency-detail.html">View Profile</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="agent agent-03 mt-4">
+          <div class="row g-0">
+            <div class="col-lg-4 text-center border-end">
+              <div class="d-flex flex-column h-100">
+                <div class="agent-avatar p-3 my-auto">
+                  <img class="img-fluid rounded-circle" src="<?=base_url('website')?>/images/agency/02.png" alt="">
+                </div>
+                <div class="agent-listing text-center mt-auto">
+                  <a href="#"><strong class="text-primary me-2 d-inline-block">12</strong>Listed Properties </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-8">
+              <div class="d-flex h-100 flex-column">
+                <div class="agent-detail">
+                  <div class="d-sm-flex">
+                    <div class="agent-name">
+                      <h5> <a href="agency-detail.html">Business estates </a></h5>
+                      <span><i class="fas fa-map-marker-alt fa-xs pe-2"></i> West Indian Summer St. Missoula</span>
+                    </div>
+                    <div class="agent-social ms-auto mt-2 mt-sm-0">
+                      <ul class="list-unstyled list-inline">
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i> </a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i> </a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin"></i> </a></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="agent-info">
+                    <p class="mt-3 mb-3">Step out on to the path to your left. Where there is no change. Briefly imagine that you are not.</p>
+                    <ul class="list-unstyled mb-0">
+                      <li><strong>Office: </strong>(123) 345-6789</li>
+                      <li><strong>Fax: </strong>(123) 345-6789</li>
+                      <li><strong>Email: </strong>support@realvilla.demo </li>
+                    </ul>
+                    <ul class="list-unstyled mb-0">
+                      <li><strong>Mobile: </strong>(456) 478-2589</li>
+                      <li><strong>WhatsApp: </strong>(456) 478-2589</li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="agent-button">
+                  <a class="btn btn-light btn-lg d-grid" href="agency-detail.html">View Profile</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="agent agent-03 mt-4">
+          <div class="row g-0">
+            <div class="col-lg-4 text-center border-end">
+              <div class="d-flex flex-column h-100">
+                <div class="agent-avatar p-3 my-auto">
+                  <img class="img-fluid rounded-circle" src="<?=base_url('website')?>/images/agency/03.png" alt="">
+                </div>
+                <div class="agent-listing text-center mt-auto">
+                  <a href="#"><strong class="text-primary me-2 d-inline-block">32</strong>Listed Properties </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-8">
+              <div class="d-flex h-100 flex-column">
+                <div class="agent-detail">
+                  <div class="d-sm-flex">
+                    <div class="agent-name">
+                      <h5> <a href="agency-detail.html">Green house </a></h5>
+                      <span><i class="fas fa-map-marker-alt fa-xs pe-2"></i> Fairground St. North Bergen, NJ</span>
+                    </div>
+                    <div class="agent-social ms-auto mt-2 mt-sm-0">
+                      <ul class="list-unstyled list-inline">
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i> </a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i> </a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin"></i> </a></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="agent-info">
+                    <p class="mt-3 mb-3">Really experience that. See what you see, hear what you hear, feel the feelings. Disappointment? Anger?</p>
+                    <ul class="list-unstyled mb-0">
+                      <li><strong>Office: </strong>(123) 345-6789</li>
+                      <li><strong>Fax: </strong>(123) 345-6789</li>
+                      <li><strong>Email: </strong>support@realvilla.demo </li>
+                    </ul>
+                    <ul class="list-unstyled mb-0">
+                      <li><strong>Mobile: </strong>(456) 478-2589</li>
+                      <li><strong>WhatsApp: </strong>(456) 478-2589</li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="agent-button">
+                  <a class="btn btn-light btn-lg d-grid" href="agency-detail.html">View Profile</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <ul class="pagination mt-5">
+              <li class="page-item disabled me-auto">
+                <span class="page-link b-radius-none">Prev</span>
+              </li>
+              <li class="page-item active" aria-current="page"><span class="page-link">1 </span> <span class="sr-only">(current)</span></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item ms-auto">
+                <a class="page-link b-radius-none" href="#">Next</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </section>
 <!--=================================
- Browse properties Categories -->
-
-<!--<section class="">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-8 mx-auto text-center mb-5">
-				<h2 style="color:red">Elements &amp; Categories</h2>
-			</div>
-		</div>
-
-<div class="row justify-content-center ">
-	<div class="col-12">
-		<div class="top_slider_holder">
-			
-				<h6 class="title_head small">
-		المجموعات التصنيفية
-	</h6>
-		<div class=" category_slider_top main owl-carousel">
-    <?php if(isset($category)){ $cnt=1; ?>  
-         <?php foreach($category as $row) { ?>
-			<div class="category_item">
-				<a class="bg-gradient shadow-hover line-draw-animation w-100 h-100 text-light d-inline-block text-primary-hover" href="<?php echo base_url('categories/').$row->id?>">
-					<div class="line-draw-inner">
-					<span><?php echo $row->c_name; ?></span>
-          <img src="<?php echo base_url('uploads/').$row->icon?>" class="image" height="40" width="40">         
-        </div>
-				</a>
-        </div> 
-        <?php } ?>    
-        <?php } ?>
-		</div> 
-
-		</div>
-	</div>
-
-
-	</div>
-
-	</div>
-</section>-->
+Listing – list view -->
